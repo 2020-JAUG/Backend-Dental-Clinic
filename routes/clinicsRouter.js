ff8a184bf783988db6aab2e589d2081803d80326
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-01 17:11:41 +0200

    create new clinic endpoint added

--- routes/clinicsRouter.js
+++ routes/clinicsRouter.js
@@ -13,3 +13,16 @@ router.get('/', async(req, res) => {
     }
 });
 
+// POST - Creates a new clinic
+
+router.post('/', async(req, res) => {
+    try {
+        const clinic = req.body;
+        res.json(await userController.createClinic(clinic))
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
