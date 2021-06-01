ae849a5111d459bf5672c9cf999048f7d99bcbe2
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-01 22:30:57 +0200

    Add path clients

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const clinicsRouter = require('./routes/clinicsRouter');
+const clientsRouter = require("./routes/clientsRouter");
 
 router.use('/clinics', clinicsRouter);
+router.use("/clients", clientsRouter);
 
 module.exports = router ;
