06dd5994c1d6ffa7fab739bcf86f3f3437e2bde8
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-02 13:20:57 +0200

    Add model client

---
+++ models/client.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const clientSchema = new Schema ({
+    name: {
+        type: "String",
+        required: true
+    },
+    email: {
+        type: "String",
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: "String",
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    city: {
+        type: "String",
+        required: true
+    },
+    cp: {
+        type: "String",
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const toJSONConfig = {
+    transform: (doc,ret,opt) => {//transform es un metodo de mongoose
+           delete ret['password']//ret es un metodo encripta la password para enviarla con mas segura
+           return ret
+    }
+}
+
+
+clientSchema.set('toJSON', toJSONConfig);
+
+const Client = mongoose.model("Client", clientSchema);
+module.exports = Client;
