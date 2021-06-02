eeadfde671de26ac09a020eae2f46d256ffd69ad
José Luis <hansokaz@gmail.com>
2021-06-02 20:35:42 +0200

    Created Appointment crud and files

--- router.js
+++ router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const clinicsRouter = require('./routes/clinicsRouter');
 const clientsRouter = require("./routes/clientsRouter");
+const appointmentRouter = require('./routes/appointmentRouter');
 
 router.use('/clinics', clinicsRouter);
 router.use("/clients", clientsRouter);
+router.use('/appointment', appointmentRouter);
 
 module.exports = router ;
