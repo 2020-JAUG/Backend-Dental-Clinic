97c36e177404cd61be35c80979c9a57904b17cee
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-02 15:57:30 +0200

    create a new dentist endpoint added

--- routes/dentistRouter.js
+++ routes/dentistRouter.js
@@ -13,4 +13,17 @@ router.get('/', async(req, res) => {
     }
 });
 
+// POST - Creates a new dentist
+
+router.post('/', async(req, res) => {
+    try {
+        const dentist = req.body;
+        res.json(await dentistController.createDentist(dentist))
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
 
