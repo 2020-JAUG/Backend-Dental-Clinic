79f634b787f2d2cde50ca66e771a77962ec3213b
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-02 16:55:51 +0200

    added dentists endpoint to router

--- router.js
+++ router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const clinicsRouter = require('./routes/clinicsRouter');
 const clientsRouter = require("./routes/clientsRouter");
+const dentistRouter = require('./routes/dentistRouter')
 
 router.use('/clinics', clinicsRouter);
 router.use("/clients", clientsRouter);
+router.use('/dentists', dentistRouter);
 
 module.exports = router ;
