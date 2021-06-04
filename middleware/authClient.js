789b7416dd9798e4673e3b14e5b0156feac51185
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-04 14:02:19 +0200

    Add middleware authClient

---
+++ middleware/authClient.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const secret = "Trigmongos team";
+
+const authenticate = (req, res, next) => {
+
+    try {
+
+        if(!req.headers.authorization){
+            // return new Error("No tienes autorización");
+            throw new Error("No tenías token");
+        }
+
+        let token = req.headers.authorization.split(' ')[1];
+
+        let auth = jwt.verify(token,secret);
+
+        if( (auth.clientId != req.body.clientId) && auth.isAdmin != true ){
+            throw new Error("No tienes permiso para realizar esta acción de usuario");
+        }
+
+        return next();
+
+    } catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+
+}
+
+
+module.exports = authenticate;
