977c5e6ca2bacca470300fc4006e3166b82a7ec6
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-08 13:35:23 +0200

    cors installed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = require('./router');
-const port = 3000;
+const port = 3005;
 const db = require("./config/mongoose");
 const cors = require('cors');
 
