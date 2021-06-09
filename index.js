61da195a0bd9607ac0e20b416d73b25fefc7c1e3
ApcarJo <hansokaz@gmail.com>
2021-06-09 12:39:54 +0200

    Changed secret word in Dentist

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const router = require('./router');
-const port = 3000;
+const port = 3006;
 const db = require("./config/mongoose");
 const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(cors());
+
 
 db
 .then(() => {
